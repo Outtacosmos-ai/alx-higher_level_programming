59ee388093981a233458f585e522437936368941
Outtacosmos-ai <messghir4@gmail.com>
2023-11-28 19:16:41 +0100

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i != ord('q') and i != ord('e'):
-        print("{:c}.format(i), end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".foramt(chr(letter)), end="")
