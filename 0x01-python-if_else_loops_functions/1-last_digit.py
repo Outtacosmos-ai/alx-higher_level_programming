bba4dcc92a9e70343f2765f0ab15365735c52833
Outtacosmos-ai <messghir4@gmail.com>
2023-11-28 14:49:57 +0100

    1-last_digit.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+
+message = "Last digit of {} is {} and is".format(number, last_digit)
+
+if last_digit > 5:
+    print(message, "greater than 5")
+elif last_digit < 6 and last_digit != 0:
+    print(message, "less than 6 and not 0")
+else:
+    print(message, "0 and is 0")
