266cd24a94bcaefad4f9e8f29cd3326e046e203e
Outtacosmos-ai <messghir4@gmail.com>
2023-11-30 14:23:22 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-if __name__ "__main__":
-    import sys
+
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+
+    # Import necessary functions and modules
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) != 4:
+    import sys
+
+    # Check the number of command-line arguments
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
+
+    # Define a dictionary mapping operators to functions
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+
+    # Check if the operator is valid
+    if sys.argv[2] not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    # Extract operands and operator from command-line arguments
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    op = sys.argv[2]
-    if op == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif op == "-":
-        print("{} - {} = {}".foramt(a, b, sub(a, b)))
-    elif op == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    else:
-        print("Unknown operator. Available operator: +, -, * and /")
-        exit(1)
+    operator = sys.argv[2]
+
+    # Perform the calculation and display the result
+    result = ops[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
