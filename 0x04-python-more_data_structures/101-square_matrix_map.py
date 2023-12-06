28dfca7045701df84037d50b0fab7c48ad8729ce
Outtacosmos-ai <messghir4@gmail.com>
2023-12-06 11:48:52 +0100

    101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    """
-    Compute the square of each element in a matrix using map.
-
-    Args:
-        matrix (list of lists): The input matrix.
-
-    Returns:
-        list of lists: Matrix with each element squared.
-    """
-    return list(map(lambda row: list(map(lambda x: x * x, row)), matrix))
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
