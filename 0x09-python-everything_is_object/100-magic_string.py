953c2f9edb3735d5b04c46d3e372f91f598d506e
Outtacosmos-ai <messghir4@gmail.com>
2024-01-06 00:30:04 +0100

    0-28

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def magic_string():
-    """Generate a magic string.
+    """
+    Generate a string with "BestSchool" repeated a certain number of times.
 
     Returns:
-    str: A string with "BestSchool" repeated a certain number of times.
+        str: A string containing "BestSchool" repeated.
     """
     magic_string.count = getattr(magic_string, 'count', 0) + 1
     return ", ".join(["BestSchool" for _ in range(magic_string.count)])
