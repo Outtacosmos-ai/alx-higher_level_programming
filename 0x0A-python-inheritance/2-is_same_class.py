efcd029624efac44a2491a82825c370a917dcebc
Outtacosmos-ai <messghir4@gmail.com>
2024-01-08 21:12:17 +0100

    inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,5 +6,4 @@ def is_same_class(obj, a_class):
     """Return true if object is an instance of the
     class, otherwise return false
     """
-    # Checks if object is an instance of the class
     return isinstance(obj, a_class)
