8552ffdedb514d284421a0f0b904ecc70a77fd17
Outtacosmos-ai <messghir4@gmail.com>
2024-01-08 21:26:25 +0100

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-"""checks if object is an instance of a class
-or an inherited class
-"""
-
+import abc
 
 def inherits_from(obj, a_class):
-    """returns true if object is an instance of a class
-    or a class that the class in question inherits from
+    """Returns true if object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False
     """
-    current_class = obj.__class__
-    while current_class is not None:
-        if current_class == a_class:
-            return True
-        current_class = current_class.__bases__[0] 
-        if current_class.__bases__ else None
-    return False
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
