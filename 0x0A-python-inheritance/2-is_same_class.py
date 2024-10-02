5de1205831b9d02e527b6df9f62f3554bfd4c16b
Outtacosmos-ai <messghir4@gmail.com>
2024-10-02 21:58:23 +0100

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,5 +4,5 @@
 
 
 def is_same_class(obj, a_class):
-    """ Compare is obj is exactly an instance of a_class """
-    return type(obj) == a_class
+    """ Compare if obj is exactly an instance of a_class """
+    return type(obj) is a_class
