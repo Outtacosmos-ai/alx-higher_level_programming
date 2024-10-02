46304ed7311fa7c1fce1cc810aa677285857642e
Outtacosmos-ai <messghir4@gmail.com>
2024-10-03 00:24:01 +0100

    0-read_file.py

--- 0x0C-python-almost_a_circle/0-read_file.py
+++ 0x0C-python-almost_a_circle/0-read_file.py
@@ -2,7 +2,7 @@
 """Module for read_file"""
 
 
-def read_file(file=""):
+def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
-    with open(file, encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
