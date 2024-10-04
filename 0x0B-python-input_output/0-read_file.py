5ab07d34a648fe622a90b13d01019ad2590bcc69
Outtacosmos-ai <messghir4@gmail.com>
2024-10-04 20:32:49 +0100

    done ✅

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """ function that reads a text file (UTF8) and prints it to stdout """
+"""Module for reading a text file and printing its content."""
 
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+
+def read_file(filename=""):
+    """Read a text file (UTF8) and print it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end='')
