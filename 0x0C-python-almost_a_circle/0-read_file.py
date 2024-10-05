79c779cbd77b724e6a6c9e5d27d1ef48bdee201c
Outtacosmos-ai <messghir4@gmail.com>
2024-10-05 01:35:16 +0100

    done ✅

--- 0x0C-python-almost_a_circle/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module for read_file"""
-
-
-def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
--- 0x0C-python-almost_a_circle/1-write_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module for write_file"""
-
-
-def write_file(filename="", text=""):
-    """Write a string to a text file (UTF8)"""
-    with open(filename, 'w', encoding="utf-8") as f:
-        return f.write(text)
